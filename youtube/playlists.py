a2863c9ea6739881ed4c68351fdd9f909728ee85
mspencersmith <mspencersmith@outlook.com>
2021-01-07 20:28:46 +0000

    Output now .csv

--- youtube/playlists.py
+++ youtube/playlists.py
@@ -1,6 +1,7 @@
 """
 Reuturns attributes of playlists 
 """
+import csv
 import os.path
 import json
 from googleapiclient.discovery import build
@@ -15,28 +16,34 @@ with open(path) as api_file:
 
 youtube = build('youtube', 'v3', developerKey=config['API_KEY'])
 
+channel_id = 'UChh-akEbUM8_6ghGVnJd6cQ'
 nextPageToken = None
+filename = 'data/bwf_playlists.csv'
+
+with open(filename, 'w') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['publishedAt', 'title', 'description', 'id'])
 
 while True:
     request = youtube.playlists().list(
         part = 'contentDetails, snippet',
-        channelId = 'UChh-akEbUM8_6ghGVnJd6cQ',
+        channelId = channel_id,
         maxResults = 50,
         pageToken = nextPageToken
         )
     response = request.execute()
     # items = response['items']
     # print(response)
-
-    for item in response['items']:
-        id_ = item['id']
-        snippet = item['snippet']
-        publishedAt = snippet['publishedAt']
-        title = snippet['title']
-        description = snippet['description']
-        print(f'{publishedAt},{title},{description},{id_}')
-        # print(item)
-        print()
+    with open(filename, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for item in response['items']:
+            id_ = item['id']
+            snippet = item['snippet']
+            publishedAt = snippet['publishedAt']
+            title = snippet['title']
+            description = snippet['description']
+            
+            csv_writer.writerow([publishedAt, title, description, id_])
 
     nextPageToken = response.get('nextPageToken')
 
