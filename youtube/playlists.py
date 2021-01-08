5044bb4f50279cd75f467badf90a62e0430bbe49
mspencersmith <mspencersmith@outlook.com>
2021-01-08 21:15:14 +0000

    Added duration column to playlists.py

--- youtube/playlists.py
+++ youtube/playlists.py
@@ -4,6 +4,8 @@ Reuturns attributes of playlists
 import csv
 import os.path
 import json
+import re
+from datetime import timedelta
 from googleapiclient.discovery import build
 from pathlib import Path
 
@@ -18,7 +20,7 @@ youtube = build('youtube', 'v3', developerKey=config['API_KEY'])
 
 channel_id = 'UChh-akEbUM8_6ghGVnJd6cQ'
 
-filename = 'data/bwf_playlists.csv'
+filename = 'data/bwf_playlists2.csv'
 
 
 
@@ -33,9 +35,10 @@ def playlist():
             csv_writer = csv.writer(csv_file)
         
             for item in response['items']:
+                # publishedAt, title, description, id_ = get_attributes(item)
                 publishedAt, title, description, id_ = get_attributes(item)
-                
-                csv_writer.writerow([publishedAt, title, description, id_])
+                duration = cal_duration(id_)
+                csv_writer.writerow([publishedAt, title, description, id_, duration])
         
         nextPageToken = response.get('nextPageToken')
 
@@ -45,7 +48,7 @@ def playlist():
 def write_header():
     with open(filename, 'w') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['publishedAt', 'title', 'description', 'id'])
+        csv_writer.writerow(['publishedAt', 'title', 'description', 'id', 'duration'])
 
 def request_playlist(nextPageToken):
 
@@ -65,5 +68,63 @@ def get_attributes(item):
     description = snippet['description']
     id_ = item['id']
     return publishedAt, title, description, id_
+
+def cal_duration(playlist_id):
+    nextPageToken = None
+
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
+
+    total_seconds = 0
+
+    while True:
+        pl_request = youtube.playlistItems().list(
+            part = 'contentDetails',
+            playlistId = playlist_id,
+            maxResults = 50,
+            pageToken = nextPageToken
+            )
+        response = pl_request.execute()
+
+        vid_ids = []
+        for item in response['items']:
+            vid_ids.append(item['contentDetails']['videoId'])
+            
+        vid_request = youtube.videos().list(
+            part = "contentDetails",
+            id=','.join(vid_ids)
+            )
+        vid_response = vid_request.execute()
+
+        for item in vid_response['items']:
+            total = item['contentDetails']['duration']
+            hours = hours_pattern.search(total)
+            minutes = minutes_pattern.search(total)
+            seconds = seconds_pattern.search(total)
+
+            hours = int(hours.group(1)) if hours else 0
+            minutes = int(minutes.group(1)) if minutes else 0
+            seconds = int(seconds.group(1)) if seconds else 0
+
+            video_seconds = timedelta(
+                hours = hours,
+                minutes = minutes,
+                seconds =  seconds
+                ).total_seconds()
+
+            total_seconds += video_seconds
+
+        nextPageToken = response.get('nextPageToken')
+
+        if not nextPageToken:
+            break
+
+    total_seconds = int(total_seconds)
+    minutes, seconds = divmod(total_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    duration = f'{hours}:{minutes}:{seconds}'
+    return duration
+
 
 playlist()
